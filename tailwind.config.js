47a3e6085b464a839ae01ed57e223e4a3e3af803
sabitha <sabithathangamani@gmail.com>
2026-03-09 16:45:47 +0530

    continued the code

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,12 @@ module.exports = {
         // "body-background":"linear-gradient(to right,#04016C,#4A16BD)",
            "body-background": "url('/images/bodybackground.png')",
         "footer-background":"url('/images/footer1img.png')",
+        "page3-background": "linear-gradient(to right, #739FE8, #4F18CA)",
+        "course-background": "linear-gradient(to right, #D9ECFF, #D9ECFF)",
+         "subscribe-background": "linear-gradient(45deg, #EFF1F4 0%, #7A68FF 56%,#E441FF 100%)",
+         "testimonals-background":"linear-gradient(to top, #DBEDFF 0%,#DBEDFF 100%)",
+         "last-background":"linear-gradient(45deg, #931BBD 10%,#477AE3 90%)",
+        "last1-background":"url('/images/last.png')",
       }
     
     },
